d7a9aba5eefa6b4bcbf09f2f39a0812b2e1f0729
Athena Ys <Stellaris5127@gmail.com>
2025-07-12 17:30:53 +0800

    update

--- open-in-obsidian.ps1
+++ open-in-obsidian.ps1
@@ -1,5 +1,4 @@
 param([string]$file)
 Add-Type -AssemblyName System.Web
 $encoded = [System.Web.HttpUtility]::UrlEncode($file)
-Start-Process "obsidian://advanced-uri?filepath=$encoded&vault=HTML5"
-exit
+start "" "obsidian://advanced-uri?filepath=$encoded&vault=HTML5"
